7e9cc9a4f15eb53d06bbbe60e8ea7492484dda4d
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-02-22 22:08:53 +0330

    #7 Has been Added IRepository and IUnitOfWork

---
+++ SuperNote.Domain/Abstractions/DataAccess/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using SuperNote.Domain.Abstractions.Aggregates;
+
+namespace SuperNote.Domain.Abstractions.DataAccess;
+
+public interface IRepository<TEntity>
+    where TEntity : AggregateRoot
+{
+    Task<IReadOnlyList<TEntity>> GetEntitiesAsync();
+    Task AddAsync(TEntity entity);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+}
