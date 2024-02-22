c99e5426a16676ba7ddeabdfeafea11eea259676
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-02-22 23:19:37 +0330

    #17 Has been Implemented Value Object for Note

---
+++ SuperNote.Domain/Notes/NoteText.cs
@@ -0,0 +1,23 @@
+﻿using FluentResults;
+
+namespace SuperNote.Domain.Notes;
+
+public sealed record NoteText
+{
+    public string? Value { get; }
+
+    private NoteText(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<NoteText> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return NoteErrors.NoteTextIsEmpty;
+        }
+
+        return Result.Ok(new NoteText(value));
+    }
+}
