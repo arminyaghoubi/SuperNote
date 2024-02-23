8927350e47cd76b459888ef445176ec176097ff2
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-02-23 11:06:06 +0330

    #31 Has been Implemented DbContext

---
+++ SuperNote.DataAccess/Contexts/SuperNoteContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperNote.Domain.Notes;
+
+namespace SuperNote.DataAccess.Contexts;
+
+public class SuperNoteContext : DbContext
+{
+    public DbSet<Note> Notes { get; set; }
+
+    public SuperNoteContext(DbContextOptions<SuperNoteContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SuperNoteContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
