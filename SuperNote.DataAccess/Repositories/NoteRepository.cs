e1ec40c0d790a9bbdfee8d5c32ce1b6bbc4341a5
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-02-23 11:53:38 +0330

    #39 Has been Implemented Note Repository

---
+++ SuperNote.DataAccess/Repositories/NoteRepository.cs
@@ -0,0 +1,22 @@
+﻿using Optional;
+using SuperNote.DataAccess.Contexts;
+using SuperNote.Domain.Notes;
+
+namespace SuperNote.DataAccess.Repositories;
+
+public class NoteRepository : Repository<Note>, INoteRepository
+{
+    public NoteRepository(SuperNoteContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<Option<Note>> GetByIdAsync(NoteId id)
+    {
+        var note = await _context
+            .Notes
+            .FindAsync(id);
+
+        return note.SomeNotNull();
+    }
+}
