0ff5b5ed50d957e941fab90a9a18192626945333
ArminYaghoubi <ArminYaghoubi1@gmail.com>
2024-02-24 17:09:36 +0330

    #47 Has been Implemented a GET API Endpoint for Get Note by Id

---
+++ SuperNote.WebApi/Endpoints/Notes/GetById/GetById.cs
@@ -0,0 +1,36 @@
+﻿using FastEndpoints;
+using MediatR;
+using SuperNote.Application.Notes.Queries.GetNoteById;
+using SuperNote.Domain.Notes;
+using SuperNote.WebApi.Extensions;
+
+namespace SuperNote.WebApi.Endpoints.Notes.GetById;
+
+public class GetById(IMediator mediator) : Endpoint<GetNoteByIdRequest, NoteDto>
+{
+    public override void Configure()
+    {
+        AllowAnonymous();
+
+        Get("/Notes/{id:guid}");
+
+        Summary(s =>
+        {
+            s.Description = "Get a note by Id.";
+        });
+    }
+
+    public override async Task HandleAsync(GetNoteByIdRequest request, CancellationToken cancellationToken)
+    {
+        var note = await mediator.Send(new GetNoteByIdQuery(new NoteId(request.Id)), cancellationToken);
+
+        if (note.IsSuccess)
+        {
+            await SendOkAsync(note.Value, cancellationToken);
+        }
+        else
+        {
+            await this.SendProblemDetailsResponse(note, cancellationToken);
+        }
+    }
+}
